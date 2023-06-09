0a4e35445917900f0d51a13fc3adf5c3b1b400f4
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-06-09 19:02:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,3 +4,38 @@
 
 touristSpots.sort();
 console.log(touristSpots);
+
+for(int I=0;i<arr.length;i++)s  += arr[I] + ' ';
+
+var arr = [....];
+
+
+var new_arr = [];
+for(int i=0;i<arr.length;i++){
+    string st = arr[i];
+    var x = st.split(' ');
+    string emp = "";
+    for(int j=0;j<x.length;j++){
+        if(x[j]=='the' || a || an)continue;
+        else {
+            emp += x[j] + ' ';
+        }
+    }
+    new_arr.push(emp);
+}
+
+//mapping 
+new_arr ==> arr
+v temple ==> the v temple
+
+
+new_arr.sort(); 
+
+for(int i=0;i<new_arr.length;i++){
+    string s = new_arr[i]; //v temple
+    string updated = mp[s];
+    new_arr[i] = updated; //the v temple
+}
+
+
+return new_arr;
